f0bd2e2e7280a432eb0b528cb56689485c414004
Sunny020720 <1538052461@qq.com>
2023-11-24 19:22:04 +0800

    配置

--- config/db.config.js
+++ config/db.config.js
@@ -6,11 +6,11 @@ mongoose.connect('mongodb://localhost:27017/article-manage', {
     useUnifiedTopology: true
 })
 // 一旦连接到 MongoDB，就可以创建用户模型并使用它将用户保存到数据库中。
-
-UserModel.create({
-    username:'jack',
-    nickname:'null',
-    password:'123456',
-    gender:0,
-    email:'1234567890@qq.com',
-    user_pic:'null' })
+//
+// UserModel.create({
+//     username:'jack',
+//     nickname:'null',
+//     password:'123456',
+//     gender:0,
+//     email:'1234567890@qq.com',
+//     user_pic:'null' })
