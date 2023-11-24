4a4c35c4e92d22d5b35d56d86ca7beea0ebf92c3
Sunny020720 <1538052461@qq.com>
2023-11-24 19:22:39 +0800

    登录接口

--- controller/users/UserController.js
+++ controller/users/UserController.js
@@ -1,16 +1,25 @@
 const UserService = require("../../services/users/UserService");
+const {generate} = require("../../util/JWT");
+
 const UserController={
     login:async(req,res)=>{
         // req.body
-        var result = await UserService.login(req.body._value)
-        // console.log(req.body._value)
+        var result = await UserService.login(req.body)
+
         if(result.length === 0){
             res.send({
-                code:"-1",
+                code: -1,
                 error:"用户名密码不匹配"
             })
         }else{
+            const token = generate({
+                _id:result[0]._id,
+                username:result[0].username
+            },'6000s')
             res.send({
+                code: 0,
+                token:token,
+                message:'登陆成功！',
                 ActionType:"OK"
             })
         }
