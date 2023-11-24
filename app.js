a14c155418172c0dcc572fcacf41e22a856e7177
Sunny020720 <1538052461@qq.com>
2023-11-24 19:22:16 +0800

    token校验

--- app.js
+++ app.js
@@ -14,10 +14,9 @@ app.use(cors())
 
 // 路由
 var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
+
 var UserRouter = require('./routes/users/UserRouter');
-
-
+const {verify, generate} = require("./util/JWT");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,10 +28,33 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// token校验
+app.use((req,res,next)=>{
+//   如果token有效，next()
+//   如果token过期，返回401错误
+  if(req.url==='/users/login'){//登录不需要token
+    next()
+    return
+  }
+  const token = req.headers['authorization'].split(' ')[1]
+  if(token){
+    const payload=verify(token)
+    if(payload){
+      const newToken = generate({
+        _id:payload._id,
+        username:payload.username
+      },'6000s')
+      res.header('Authorization',newToken)
+      next()
+    }else {
+      res.status(401).send({code:-1,message:'token过期'})
+    }
+  }
+})
+
 // 路由
 app.use('/', indexRouter);
 app.use('/index', indexRouter);
-// app.use('/users', usersRouter);
 app.use(UserRouter)
 
 // catch 404 and forward to error handler
