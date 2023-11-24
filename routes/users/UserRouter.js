baa0d65a1813c5a637337d934bfccf4e2201a68e
Sunny020720 <1538052461@qq.com>
2023-11-24 19:22:53 +0800

    登录路径

--- routes/users/UserRouter.js
+++ routes/users/UserRouter.js
@@ -2,10 +2,6 @@ var express = require('express');
 var UserRouter = express.Router();
 const UserController = require('../../controller/users/UserController');
 
-// UserRouter.get('/wly/users/login', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-UserRouter.post("/wly/users/login",UserController.login)
+UserRouter.post("/users/login",UserController.login)
 
 module.exports = UserRouter;
